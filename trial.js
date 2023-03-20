b7b4f795ea77fbc4b6b7e11211b0070750265358
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-03-20 11:00:17 +0300

    new Set in js file

--- trial.js
+++ trial.js
@@ -1,7 +1,7 @@
-let arr1 = [4,3,5,6,7]
-let arr2 = [3,4]
+let arr1 = [4,3,5,5,5,5,5,5,5,5,5,5,6,7]
+let arr2 = [3,4,5,5,5,5,5,5,5]
 let newarr = [...arr1, ...arr2];
 console.log(newarr)
 
-let arr3 = new Set([1,2,2,3])
+let arr3 = [...new Set(arr1, ...arr2)]
 console.log(arr3)
